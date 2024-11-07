18634a2b50129e65226d3ba9e4eb4f9da972d37b
Francis GR <umnomemuitocriativo@gmail.com>
2024-11-07 23:52:49 +0000

    feat: add environment variables to environment variables array

--- src/env.c
+++ src/env.c
@@ -12,35 +12,36 @@
 
 #include "minishell.h"
 
-char	**env_dup(char **env)
+char	**env_dup(char **env, t_terminal *t)
 {
 	char	**env_dup;
-	size_t	size;
+	size_t	capacity;
 	size_t	i;
 
 	if (!env)
 		return (NULL);
-	size = 200;
-	env_dup = ft_calloc(size, sizeof(char *) + 1);
+	capacity = ENV_INITIAL_SIZE;
+	env_dup = ft_calloc(capacity, sizeof(char *) + 1);
 	if (!env_dup)
 		return (NULL);
 	i = 0;
 	while (env[i])
 	{
-		// TODO: substituir por env_add()
-		if (i > size)
+		if (i == capacity - 1)
 		{
-			size *= 2;
-			env_dup = (char **)ft_realloc(env_dup, size / 2, size);
+			capacity *= 2;
+			env_dup = (char **)ft_realloc(env_dup, i, capacity);
 		}
 		env_dup[i] = ft_strndup(env[i], ft_strlen(env[i]));
 		i++;
 	}
+	t->env_capacity = capacity;
+	t->env_size = i;
 	env_dup[i] = NULL;
 	return (env_dup);
 }
 
-char	*env_lookup(char **env, char *key)
+char	*env_lookup(char **env, char *key, int should_lookup)
 {
 	size_t	key_len;
 	char	*equals_ptr;
@@ -60,8 +61,60 @@ char	*env_lookup(char **env, char *key)
 		equals_ptr = ft_strchr(*env, '=');
 		if (equals_ptr && (size_t)(equals_ptr - *env) == key_len
 			&& ft_strncmp(*env, key, key_len) == 0)
-			return (++equals_ptr);
+		{
+			if (should_lookup == VALUE)
+				return (++equals_ptr);
+			else
+				return (*env);
+		}
 		env++;
 	}
 	return (NULL);
+}
+
+//int setenv(const char *name, const char *value, int overwrite);
+void env_set(char **env, char *key, char *new_value, t_terminal *t)
+{
+	char *key_exists;
+	char *new_key_and_value;
+
+	if (!t || !t->env || !key || !new_value)
+		return ;
+	key_exists = env_lookup(t->env, key, VALUE);
+	if (key_exists)
+	{
+		if (ft_strlen(key_exists) > new_value)
+			ft_strlcpy(key_exists, new_value, ft_strlen(new_value) + 1);
+		else
+		{
+			new_key_and_value = malloc(ft_strlen(new_value) + ft_strlen(key) + 2);
+			ft_strlcpy(new_key_and_value, key, ft_strlen(key) + 1);
+			ft_strlcat(new_key_and_value, "=", 2);
+			ft_strlcat(new_key_and_value, new_value, ft_strlen(new_value) + 1);
+			return ;
+		}
+	}
+	else if (t->env_size + 1 < t->env_capacity)
+	{
+		new_key_and_value = malloc(ft_strlen(new_value) + ft_strlen(key) + 2);
+		ft_strlcpy(new_key_and_value, key, ft_strlen(key) + 1);
+		ft_strlcat(new_key_and_value, "=", 2);
+		ft_strlcat(new_key_and_value, new_value, ft_strlen(new_value) + 1);
+		env[t->env_size] = new_key_and_value;
+		env[t->env_size + 1] = NULL;
+		t->env_size++;
+		return ;
+	}
+	else
+	{
+		t->env_capacity *= 2;
+		ft_realloc(env, t->env_size, t->env_capacity);
+		new_key_and_value = malloc(ft_strlen(new_value) + ft_strlen(key) + 2);
+		ft_strlcpy(new_key_and_value, key, ft_strlen(key) + 1);
+		ft_strlcat(new_key_and_value, "=", 2);
+		ft_strlcat(new_key_and_value, new_value, ft_strlen(new_value) + 1);
+		env[t->env_size] = new_key_and_value;
+		env[t->env_size + 1] = NULL;
+		t->env_size++;
+	}
 }
