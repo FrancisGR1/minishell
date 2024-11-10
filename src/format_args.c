07395fa9412a53d701d4bef8b5a49a661669050e
Francis GR <umnomemuitocriativo@gmail.com>
2024-11-10 13:44:25 +0000

    refactor(expansion): args rearrangement - shorter func

--- src/format_args.c
+++ src/format_args.c
@@ -68,79 +68,28 @@ void	alloc_args(t_cmd *cmds, int commands_num, char **t_env)
 	}
 }
 
-//TODO: fazer func que copia args
 t_string *make_rearranged_args(t_string *old, t_string *split, int cur, int c)
 {
     t_string *na;
-    t_string *old_duped;
     const int len = strs_count(split);
     
-    if (!old || !split)
-        return (NULL);
-        
-    // Allocate new array
     na = malloc((c + 1) * sizeof(t_string));
-    if (!na)
-        return NULL;
-        
-    // Create deep copy of old array
-    old_duped = str_arr_dup(old);
-    if (!old_duped)
-    {
-        free(na);
-        return NULL;
-    }
-    
-    // Free original array
-    string_arr_free(&old);
-    
-    // We need to deep copy the strings when copying to na
+    if (!old || !split || !na)
+        return (NULL);
     if (cur == 0)
     {
-        // Copy split strings with deep copy
-        for (int i = 0; i < len; i++)
-            na[i] = str_dup(split[i]);
-            
-        // Copy remaining strings from old_duped
-        if (c > 1)
-            for (int i = 0; i < (c - len); i++)
-                na[len + i] = str_dup(old_duped[1 + i]);
-    }
-    else if (cur == c - 1)
-    {
-        // Copy prefix from old_duped
-        for (int i = 0; i < (c - 1); i++)
-            na[i] = str_dup(old_duped[i]);
-            
-        // Copy split strings at the end
-        for (int i = 0; i < len; i++)
-            na[c - 1 + i] = str_dup(split[i]);
+	    str_arr_deep_copy(na, split, len);
+	    str_arr_deep_copy(&na[len], &old[1], c - len);
     }
     else
     {
-        // Copy prefix from old_duped
-        for (int i = 0; i < cur; i++)
-            na[i] = str_dup(old_duped[i]);
-            
-        // Copy split strings in the middle
-        for (int i = 0; i < len; i++)
-            na[cur + i] = str_dup(split[i]);
-            
-        // Copy suffix from old_duped if needed
-        if (c > (cur + len))
-        {
-            for (int i = 0; i < (c - cur - len); i++)
-                na[cur + len + i] = str_dup(old_duped[cur + 1 + i]);
-        }
+	    str_arr_deep_copy(na, old, cur);
+	    str_arr_deep_copy(&na[cur], split, len);
+	    str_arr_deep_copy(&na[cur + len], &old[cur + 1], c-cur-len);
     }
-    
-    // Free the temporary copy
-    string_arr_free(&old_duped);
-    
-    // Set NULL terminator
+    string_arr_free(&old);
     na[c] = new_str(NULL, 0);
-    
-    return na;
+    return (na);
 }
 
 void	rearrange_args_after_expansion(t_string **arg, int current,
@@ -156,7 +105,6 @@ void	rearrange_args_after_expansion(t_string **arg, int current,
 	if (*(*arg)->s == '\'' || *(*arg)->s == '\"')
 		return ;
 	expanded_str_ptr = *arg + current;
-	debug_args("expanded_str_ptr", expanded_str_ptr);
 	if (!expanded_str_ptr)
 		return ;
 	split_args = string_split_dup(*expanded_str_ptr, " ", &arr_len);
