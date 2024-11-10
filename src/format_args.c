9da54642093cd36861b5d149c2a649368cfd5818
Francis GR <umnomemuitocriativo@gmail.com>
2024-11-10 12:56:05 +0000

    rework(expansion): rearrangement now deep copies instead of copying memory

--- src/format_args.c
+++ src/format_args.c
@@ -69,42 +69,78 @@ void	alloc_args(t_cmd *cmds, int commands_num, char **t_env)
 }
 
 //TODO: fazer func que copia args
-t_string	*make_rearranged_args(t_string *old, t_string *split, int cur,
-		int c)
+t_string *make_rearranged_args(t_string *old, t_string *split, int cur, int c)
 {
-	t_string	*na;
-	t_string	*old_duped;
-	const int	len = strs_count(split);
-
-	if (!old || !split)
-		return (NULL);
-
-	na = malloc((c + 1) * sizeof(t_string));
-	//TODO: dá leaks
-	old_duped = str_arr_dup(old);
-	string_arr_free(&old);
-	if (cur == 0)
-	{
-		ft_memcpy(na, split, sizeof(t_string) * len);
-		if (c > 1)
-			ft_memcpy(&na[len], &old_duped[1], sizeof(t_string) * (c - len));
-	}
-	else if (cur == c - 1)
-	{
-		ft_memcpy(na, old_duped, sizeof(t_string) * (c - 1));
-		ft_memcpy(&na[c - 1], split, sizeof(t_string) * len);
-	}
-	else
-	{
-		ft_memcpy(na, old_duped, sizeof(t_string) * cur);
-		ft_memcpy(&na[cur], split, sizeof(t_string) * len);
-		if (c > (cur + len))
-			ft_memcpy(&na[cur + len], old_duped + cur + 1, sizeof(t_string) * (c - cur - len));
-	}
-	//TODO: dá leaks se comentar, dá erros de escrita de descomentar
-	//string_arr_free(&old_duped);
-	na[c] = new_str(NULL, 0);
-	return (na);
+    t_string *na;
+    t_string *old_duped;
+    const int len = strs_count(split);
+    
+    if (!old || !split)
+        return (NULL);
+        
+    // Allocate new array
+    na = malloc((c + 1) * sizeof(t_string));
+    if (!na)
+        return NULL;
+        
+    // Create deep copy of old array
+    old_duped = str_arr_dup(old);
+    if (!old_duped)
+    {
+        free(na);
+        return NULL;
+    }
+    
+    // Free original array
+    string_arr_free(&old);
+    
+    // We need to deep copy the strings when copying to na
+    if (cur == 0)
+    {
+        // Copy split strings with deep copy
+        for (int i = 0; i < len; i++)
+            na[i] = str_dup(split[i]);
+            
+        // Copy remaining strings from old_duped
+        if (c > 1)
+            for (int i = 0; i < (c - len); i++)
+                na[len + i] = str_dup(old_duped[1 + i]);
+    }
+    else if (cur == c - 1)
+    {
+        // Copy prefix from old_duped
+        for (int i = 0; i < (c - 1); i++)
+            na[i] = str_dup(old_duped[i]);
+            
+        // Copy split strings at the end
+        for (int i = 0; i < len; i++)
+            na[c - 1 + i] = str_dup(split[i]);
+    }
+    else
+    {
+        // Copy prefix from old_duped
+        for (int i = 0; i < cur; i++)
+            na[i] = str_dup(old_duped[i]);
+            
+        // Copy split strings in the middle
+        for (int i = 0; i < len; i++)
+            na[cur + i] = str_dup(split[i]);
+            
+        // Copy suffix from old_duped if needed
+        if (c > (cur + len))
+        {
+            for (int i = 0; i < (c - cur - len); i++)
+                na[cur + len + i] = str_dup(old_duped[cur + 1 + i]);
+        }
+    }
+    
+    // Free the temporary copy
+    string_arr_free(&old_duped);
+    
+    // Set NULL terminator
+    na[c] = new_str(NULL, 0);
+    
+    return na;
 }
 
 void	rearrange_args_after_expansion(t_string **arg, int current,
@@ -120,6 +156,7 @@ void	rearrange_args_after_expansion(t_string **arg, int current,
 	if (*(*arg)->s == '\'' || *(*arg)->s == '\"')
 		return ;
 	expanded_str_ptr = *arg + current;
+	debug_args("expanded_str_ptr", expanded_str_ptr);
 	if (!expanded_str_ptr)
 		return ;
 	split_args = string_split_dup(*expanded_str_ptr, " ", &arr_len);
@@ -127,7 +164,6 @@ void	rearrange_args_after_expansion(t_string **arg, int current,
 		return (string_arr_free(&split_args));
 	*argc += arr_len - 1;
 	new_args = make_rearranged_args(*arg, split_args, current, *argc);
-	//string_free((t_string *)expanded_str_ptr);
-	free(split_args);
+	string_arr_free(&split_args);
 	*arg = new_args;
 }
