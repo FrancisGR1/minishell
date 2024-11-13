347bd5791dc36763dc05f2d8022a594cd19e4e51
Francis GR <umnomemuitocriativo@gmail.com>
2024-11-13 19:54:18 +0000

    tweak(execution): if command only contains empty expanded args, DON'T execute

--- src/exec_utils.c
+++ src/exec_utils.c
@@ -6,11 +6,65 @@
 /*   By: frmiguel <frmiguel@student.42Lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 20:03:47 by frmiguel          #+#    #+#             */
-/*   Updated: 2024/11/06 21:57:14 by frmiguel         ###   ########.fr       */
+/*   Updated: 2024/11/13 00:20:16 by frmiguel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+bool	is_builtin(char *command)
+{
+	static const char	*builtins[] = {"echo", "unset", "env", "export", "pwd",
+		"exit", "cd", NULL};
+	size_t				i;
+
+	i = 0;
+	while (builtins[i])
+	{
+		if (ft_strcmp(command, builtins[i]) == 0)
+			return (true);
+		i++;
+	}
+	return (false);
+}
+
+int	exec_builtin(char **argv, int argc, t_terminal *t)
+{
+	int	exit;
+
+	if (!argv || !argv[0] || !t)
+		return (0);
+	exit = 0;
+	if (!ft_strcmp(argv[0], "echo"))
+		exit = builtin_echo(argv, argc);
+	else if (!ft_strcmp(argv[0], "unset"))
+		exit = builtin_unset(argv, argc, t);
+	else if (!ft_strcmp(argv[0], "env"))
+		exit = builtin_env(t);
+	else if (!ft_strcmp(argv[0], "export"))
+		exit = builtin_export(argv, argc, t);
+	else if (!ft_strcmp(argv[0], "pwd"))
+		exit = builtin_pwd();
+	else if (!ft_strcmp(argv[0], "exit"))
+		exit = builtin_exit(argv, argc, t);
+	else if (!ft_strcmp(argv[0], "cd"))
+		exit = builtin_cd(argv, argc, t);
+	if (!should_exec_in_main(argv, t))
+		freexit(exit, t);
+	return (exit);
+}
+
+bool	should_exec_in_main(char **argv, t_terminal *t)
+{
+	bool	heredoc_no_error;
+
+	if (!argv || !t)
+		return (false);
+	heredoc_no_error = t->cmds && t->cmds[0].ri.heredoc_wstatus < FATAL_ERROR;
+	return (t->cmds_num == 1 && heredoc_no_error && (!ft_strcmp(argv[0], "cd")
+			|| !ft_strcmp(argv[0], "exit") || (!ft_strcmp(argv[0], "export")
+				&& t->cmds->cstr_argc > 1) || !ft_strcmp(argv[0], "unset")));
+}
 
 void	dup2_pipe(int fds[][2], int idx, int last)
 {
